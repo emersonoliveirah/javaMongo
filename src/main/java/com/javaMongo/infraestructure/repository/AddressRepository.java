92a5e90f490b0ea7f3891d7db565f272075e1a71
emersonoliveirah <emersonoliveirah@gmail.com>
2025-01-17 22:41:20 -0300

    UserRepository and AddressRepository.

--- src/main/java/com/javaMongo/infraestructure/repository/AddressRepository.java
+++ src/main/java/com/javaMongo/infraestructure/repository/AddressRepository.java
@@ -2,6 +2,12 @@ package com.javaMongo.infraestructure.repository;
 
 import com.javaMongo.infraestructure.entity.AddressEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface AddressRepository extends MongoRepository<AddressEntity, String> {
+
+        AddressEntity findByUserId(String userId);
+
+        @Transactional
+        void deleteByUserId(String userId);
 }
--- src/main/java/com/javaMongo/infraestructure/repository/UserRepository.java
+++ src/main/java/com/javaMongo/infraestructure/repository/UserRepository.java
@@ -2,6 +2,13 @@ package com.javaMongo.infraestructure.repository;
 
 import com.javaMongo.infraestructure.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface UserRepository extends MongoRepository<UserEntity, String> {
+
+    UserEntity findByEmail(String email);
+
+    @Transactional
+    static void deleteByEmail(String email) {
+    }
 }
